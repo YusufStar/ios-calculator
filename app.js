d77b1a0db84aa919ae25a5b2d78cd410ec9e8972
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-09-25 09:55:23 +0300

    .

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let islem;
 btncontainer.addEventListener("click", (e) => {
     console.log(e)
     if(e.path.length== 7){
+
         if(e.target.innerHTML == "=") {
             currentresult.innerHTML = eval(currentresult.innerHTML)
             sethistory()
@@ -27,10 +28,28 @@ btncontainer.addEventListener("click", (e) => {
     
 })
 
+const map = ["0","1","2","3","4","5","6","7","8","9","/","*","-","+","%","."]
+
 window.addEventListener("keydown", (x) => {
+
+    for(let i = 0;i < map.length; i++) {
+        if(x.key == map[i]) {
+            currentresult.innerHTML+=x.key
+            islem = currentresult.innerHTML
+            break
+        } else {
+            if(x.key == "Enter") {
+                currentresult.innerHTML = eval(currentresult.innerHTML)
+                sethistory()
+                break
+            }
+        }
+    }
+
     if(x.keyCode == 8) {
        if(currentresult.innerHTML.length > 0) {
         currentresult.innerHTML = currentresult.innerHTML.slice(0, -1)
+        islem = currentresult.innerHTML
        }
     }
 })
